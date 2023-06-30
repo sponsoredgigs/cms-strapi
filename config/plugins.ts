83b13b5cbf66bef0fa94994739607dce1c2d33df
yurisasc <yuris_aryansyah@yahoo.com>
2023-06-30 22:03:35 +1000

    first commit

---
+++ config/plugins.ts
@@ -0,0 +1,31 @@
+export default ({ env }) => ({
+  ckeditor: {
+    enabled: true,
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
